e2b3e3d3f57f8a58029c9c18afd772dadd2b138c
adabassiii1 <ada.bassiii1@gmail.com>
2026-07-14 13:06:08 +0200

    code correction feature-filter-by-age

--- filter_users.py
+++ filter_users.py
@@ -14,7 +14,7 @@ def filter_users_by_age(age):
   with open("users.json", "r") as file:
       users = json.load(file)
 
-  filtered_users = [user for user in users if user.get("age") == age]
+  filtered_users = [user for user in users if user.get("age") == int(age)]
 
   for user in filtered_users:
       print(user)
